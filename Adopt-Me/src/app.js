b36dfd3bacffbdd93f9762fcfecdabee3abb4dee
Pei Ern Koh <peiern.28@gmail.com>
2021-12-17 16:33:05 +0800

    create Pet component

--- Adopt-Me/src/app.js
+++ Adopt-Me/src/app.js
@@ -1,10 +1,24 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, "Luna"),
+    React.createElement("h3", {}, "Dog"),
+    React.createElement("h3", {}, "Havanese")
+  ])
+}
+
+
 // App is a function that returns the result of React.createElement
 // App is a React component -> kinda like a class of components and we can create instances of it
 const App = () => {
   return React.createElement(
     "div",
     {},
-    React.createElement("h1", {}, "Adopt Me!")
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet),
+      React.createElement(Pet),
+      React.createElement(Pet)
+    ]
   );
 };
 
