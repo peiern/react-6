82dded3048e0e392a0efcbeb1b54291c248e361b
Pei Ern Koh <peiern.28@gmail.com>
2021-12-17 18:37:04 +0800

    refactored components to jsx style

--- Adopt-Me/src/Pet.js
+++ Adopt-Me/src/Pet.js
@@ -1,11 +1,23 @@
-import React from 'react';
 // props = properties. props are what is passed down from one parent component to a child component
+// js way of writing
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, props.name),
+//     React.createElement("h3", {}, props.animal),
+//     React.createElement("h3", {}, props.breed),
+//   ]);
+// };
+
+// jsx way of writing.
+// 👇 will generate the same result as the above code
 const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+  return(
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  )
+}
 
 export default Pet;
--- Adopt-Me/src/app.js
+++ Adopt-Me/src/app.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from './pet';
 // data flows downwards in React (one way data flow)
@@ -6,25 +5,36 @@ import Pet from './pet';
 
 // App is a function that returns the result of React.createElement
 // App is a React component -> kinda like a class of components and we can create instances of it
+// const App = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Sudo",
+//       animal: "Dog",
+//       breed: "Wheaten Terrier",
+//     }),
+//   ]);
+// };
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  );
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root")); // the createElement here is to create the instance of App
