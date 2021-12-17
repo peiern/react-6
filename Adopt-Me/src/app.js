4bc46fe3cfbf29adeefe6b7758561de68c2476e4
Pei Ern Koh <peiern.28@gmail.com>
2021-12-17 17:16:29 +0800

    init npm and installed prettier

--- Adopt-Me/src/app.js
+++ Adopt-Me/src/app.js
@@ -6,23 +6,31 @@ const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h2", {}, props.name),
     React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed)
-  ])
-}
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
 
 // App is a function that returns the result of React.createElement
 // App is a React component -> kinda like a class of components and we can create instances of it
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese"}),
-      React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
-      React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Wheaten Terrier" })
-    ]
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Sudo",
+      animal: "Dog",
+      breed: "Wheaten Terrier",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root')) // the createElement here is to create the instance of App
+ReactDOM.render(React.createElement(App), document.getElementById("root")); // the createElement here is to create the instance of App
