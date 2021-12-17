5f21e7e5c0f2bcc7359ff369214ec0e30c01e482
Pei Ern Koh <peiern.28@gmail.com>
2021-12-17 16:41:27 +0800

    props added for Pet

--- Adopt-Me/src/app.js
+++ Adopt-Me/src/app.js
@@ -1,11 +1,14 @@
-const Pet = () => {
+// data flows downwards in React (one way data flow)
+// goes from parent to child. child has no way of messing with the parent component
+
+// props = properties. props are what is passed down from one parent component to a child component
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h2", {}, "Luna"),
-    React.createElement("h3", {}, "Dog"),
-    React.createElement("h3", {}, "Havanese")
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed)
   ])
 }
-
 
 // App is a function that returns the result of React.createElement
 // App is a React component -> kinda like a class of components and we can create instances of it
@@ -15,9 +18,9 @@ const App = () => {
     {},
     [
       React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet)
+      React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese"}),
+      React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
+      React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Wheaten Terrier" })
     ]
   );
 };
