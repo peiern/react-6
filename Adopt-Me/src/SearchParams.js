359a1ff445fa2d27450aba93ac6b4ac64ff0a874
Pei Ern Koh <peiern.28@gmail.com>
2021-12-20 16:52:02 +0800

    set up animal hook

--- Adopt-Me/src/SearchParams.js
+++ Adopt-Me/src/SearchParams.js
@@ -2,9 +2,11 @@ import { useState } from 'react';
 // useState is a hook that allows us to keep track of state as indicated by the state
 // hooks always begins with 'use'
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA"); // this is a hook that allows us to have the location and setLocation will track it over time
+  const [animal, setAnimal] = useState("");
   return (
     <div className="search-params">
       <form>
@@ -16,6 +18,25 @@ const SearchParams = () => {
           value={location}
           placeholder="Location" />
         </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select id="animal"
+          onChange={(e) => setAnimal(e.target.value)} // with only onChange here, it gives an error to use onBlur instead as it worries onChange might not be enough to capture keyboard or screen reader users' response
+          onBlur={(e) => setAnimal(e.target.value)}
+          value={animal} >
+
+          <option />
+          {
+            ANIMALS.map(animal => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))
+          }
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
