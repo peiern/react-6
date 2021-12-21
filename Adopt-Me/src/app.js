3df8707d1e005e6d510310d1ab786e3ba0e11fba
Pei Ern Koh <peiern.28@gmail.com>
2021-12-21 13:40:47 +0800

    added routes in App.js

--- Adopt-Me/src/app.js
+++ Adopt-Me/src/app.js
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-// import Pet from './Pet';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchParams from './SearchParams';
+import Details from './Details';
 // data flows downwards in React (one way data flow)
 // goes from parent to child. child has no way of messing with the parent component
 
@@ -32,10 +33,17 @@ const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" /> */}
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            {/* 👆 :id is a variable of id */}
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
